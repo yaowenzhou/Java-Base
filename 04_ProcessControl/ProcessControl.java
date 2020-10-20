24d4680e22aac04126c36cbaa88624b3eca79261
姚文洲 <60352284+yaowenzhou@users.noreply.github.com>
2020-10-20 17:46:33 +0800

    new notes

---
+++ 04_ProcessControl/ProcessControl.java
@@ -0,0 +1,11 @@
+public class ProcessControl {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        if (x == y) {
+            System.out.println(x + " == " + y);
+        } else {
+            System.out.println(x + " != " + y);
+        }
+    }
+}
---
+++ Others/ScannerTest.java
@@ -0,0 +1,17 @@
+/*
+    从键盘获取不同类型的变量，需要使用Scanner类
+    具体实现步骤：
+    1. 导包： import java.util.Scanner;
+    2. Scanner 的实例化
+    3. 调用Scanner类的相关方法
+*/
+import java.util.Scanner;
+
+class ScannerTest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please input a number...");
+        int num = scan.nextInt();
+        System.out.println(num);
+    }
+}
