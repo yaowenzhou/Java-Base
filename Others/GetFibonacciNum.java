00a18fcf79fbfdc1891fe415782a1710bcc7d603
姚文洲 <60352284+yaowenzhou@users.noreply.github.com>
2020-10-21 20:02:25 +0800

    Non-recursive solution of Fibonacci numbers

---
+++ Others/GetFibonacciNum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class GetFibonacciNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input a integer:");
+        int N = scanner.nextInt();
+
+        switch (N) {
+            case 0:
+                System.out.println(0);
+                return;
+            case 1:
+                System.out.println(1);
+                return;
+            case 2:
+                System.out.println(2);
+                return;
+        }
+        int sum_1 = 2;
+        int sum_2 = 1;
+        int sum = 0;
+        for (int i = 3; i <= N; i++) {
+            sum = sum_1 + sum_2;
+            sum_2 = sum_1;
+            sum_1 = sum;
+        }
+        System.out.println(sum);
+    }
+}
