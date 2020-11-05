4fd7239334c480ef6eb1c6bc24c7c8f299bf5b42
姚文洲 <60352284+yaowenzhou@users.noreply.github.com>
2020-11-05 20:04:23 +0800

    add code

---
+++ 05_ArraysToolClass/ArraysTest.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+/* java.util.Arrays:
+ *  Tool class for manipulating arrays
+ *  操作数组的工具类
+ *  Defines many methods to manipulate arrays
+ *  定义了很多操作数组的方法
+*/
+public class ArraysTest {
+    
+}
