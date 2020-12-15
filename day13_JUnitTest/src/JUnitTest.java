8b314973d5d1b4b3ff7036e0faf31b58df37d2a5
yaowenzhou <yaowenzhou@foxmail.com>
2020-12-15 21:08:27 +0800

    junit test demo

---
+++ day13_JUnitTest/src/JUnitTest.java
@@ -0,0 +1,20 @@
+/**
+ * @Package      : []
+ * @Author       : yaowenzhou
+ * @Date         : 2020-12-15 21:04:52 Tuesday
+ * @LastEditors  : yaowenzhou
+ * @LastEditTime : 2020-12-15 21:06:40
+ * @Version      : 
+ * @Description  : 
+ */
+
+import org.junit.Test;
+
+public class JUnitTest {
+    @Test
+    public void testEquals() {
+        String s1 = "MM";
+        String s2 = "MM";
+        System.out.println(s1.equals(s2));
+    }
+}
