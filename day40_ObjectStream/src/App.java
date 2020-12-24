bbfc51e54d31c2c3bfe526bb8b37ae7136186a02
姚文洲 <60352284+yaowenzhou@users.noreply.github.com>
2020-12-24 18:10:32 +0800

    object stream

---
+++ day40_ObjectStream/src/App.java
@@ -0,0 +1,110 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.junit.Test;
+
+/**
+ * 对象流的使用
+ * 1. ObjectInputStream和ObjectOutputStream
+ * 2. 作用: 存储和读取基本数据类型或对象的处理流
+ *      它的强大之处在于可以把Java中的对象写入到数据源中
+ *      也能把对象从数据源中还原回来
+ */
+
+public class App{
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    // 序列化
+    @Test
+    public void ObjectOutputStreamTest() {
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(new FileOutputStream("E:\\Code\\Java-Base\\day40_ObjectStream\\object.dat"));
+            oos.writeObject(new String("I love programming!\n我爱编程!"));
+            oos.flush();
+        } catch (IOException e) {
+        } finally {
+            try {
+                if (oos != null)
+                    oos.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    // 反序列化
+    @Test
+    public void ObjectInputStreamTest() {
+        ObjectInputStream ois = null;
+        try {
+            ois = new ObjectInputStream(new FileInputStream("E:\\Code\\Java-Base\\day40_ObjectStream\\object.dat"));
+            Object obj = ois.readObject();
+            String str = (String) obj;
+            System.out.println(str);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (ois != null) {
+                try {
+                    ois.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    // Serialization of custom classes
+    // 自定义类的序列化
+    // To a certain class of objects can be serialized,
+    // you must implement the interface Serializable or Externalizable
+    // 要想某个类的对象可序列化，必须实现Serializable或者Externalizable接口
+    // And the current class needs to provide a global constant: serialVersionUID
+    // 并且当前累需要提供一个全局常量: serialVersionUID
+    @Test
+    public void ObjectOutputStreamTest1() {
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(new FileOutputStream("E:\\Code\\Java-Base\\day40_ObjectStream\\Person.dat"));
+            oos.writeObject(new Person("xiaoyao", 25));
+            oos.flush();
+        } catch (IOException e) {
+        } finally {
+            try {
+                if (oos != null)
+                    oos.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+    // Deserialization of custom classes
+    // 自定义类的反序列化
+    @Test
+    public void ObjectInputStreamTest1() {
+        ObjectInputStream ois = null;
+        try {
+            ois = new ObjectInputStream(new FileInputStream("E:\\Code\\Java-Base\\day40_ObjectStream\\Person.dat"));
+            Object obj = ois.readObject();
+            Person str = (Person) obj;
+            System.out.println(str);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (ois != null) {
+                try {
+                    ois.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ day40_ObjectStream/src/Person.java
@@ -0,0 +1,91 @@
+import java.io.Serializable;
+
+public class Person implements Serializable{
+    private static final long serialVersionUID = 4735594836042950963L;
+    private String name;
+    private int age;
+
+    /**
+     * Parameterless constructor
+     */
+    public Person() {
+    }
+
+    /**
+     * @param name
+     * @param age
+     */
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "Person [age=" + age + ", name=" + name + "]";
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + age;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Person))
+            return false;
+        Person other = (Person) obj;
+        if (age != other.age)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+}
